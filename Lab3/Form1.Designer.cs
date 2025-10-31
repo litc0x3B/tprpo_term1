293b54342ed755d67fb4c31bf620c5044e122597
litc0x3B <91330335+litc0x3B@users.noreply.github.com>
2025-10-31 04:09:48 +0300

    Лаб3: Сделал первая версия

---
+++ Lab3/Form1.Designer.cs
@@ -0,0 +1,152 @@
+﻿namespace Lab3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.addListToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.addElementToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.buttonEnumToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.objectEnumToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.menuStrip1.SuspendLayout();
+            this.statusStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.BackColor = System.Drawing.SystemColors.Control;
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.addListToolStripMenuItem,
+            this.addElementToolStripMenuItem,
+            this.buttonEnumToolStripMenuItem,
+            this.objectEnumToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.RenderMode = System.Windows.Forms.ToolStripRenderMode.Professional;
+            this.menuStrip1.Size = new System.Drawing.Size(544, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // addListToolStripMenuItem
+            // 
+            this.addListToolStripMenuItem.Name = "addListToolStripMenuItem";
+            this.addListToolStripMenuItem.Size = new System.Drawing.Size(104, 20);
+            this.addListToolStripMenuItem.Text = "Создать список";
+            this.addListToolStripMenuItem.Click += new System.EventHandler(this.addListToolStripMenuItem_Click);
+            // 
+            // addElementToolStripMenuItem
+            // 
+            this.addElementToolStripMenuItem.Name = "addElementToolStripMenuItem";
+            this.addElementToolStripMenuItem.Size = new System.Drawing.Size(120, 20);
+            this.addElementToolStripMenuItem.Text = "Добавить элемент";
+            this.addElementToolStripMenuItem.Click += new System.EventHandler(this.addElementToolStripMenuItem_Click);
+            // 
+            // buttonEnumToolStripMenuItem
+            // 
+            this.buttonEnumToolStripMenuItem.Name = "buttonEnumToolStripMenuItem";
+            this.buttonEnumToolStripMenuItem.Size = new System.Drawing.Size(111, 20);
+            this.buttonEnumToolStripMenuItem.Text = "Перебор кнопок";
+            this.buttonEnumToolStripMenuItem.Click += new System.EventHandler(this.buttonEnumToolStripMenuItem_Click);
+            // 
+            // objectEnumToolStripMenuItem
+            // 
+            this.objectEnumToolStripMenuItem.Name = "objectEnumToolStripMenuItem";
+            this.objectEnumToolStripMenuItem.Size = new System.Drawing.Size(122, 20);
+            this.objectEnumToolStripMenuItem.Text = "Перебор объектов";
+            this.objectEnumToolStripMenuItem.Click += new System.EventHandler(this.objectEnumToolStripMenuItem_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.panel1.AutoScroll = true;
+            this.panel1.BackColor = System.Drawing.SystemColors.Control;
+            this.panel1.Location = new System.Drawing.Point(0, 24);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(544, 231);
+            this.panel1.TabIndex = 1;
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.BackColor = System.Drawing.SystemColors.Control;
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabel1});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 258);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.RenderMode = System.Windows.Forms.ToolStripRenderMode.Professional;
+            this.statusStrip1.Size = new System.Drawing.Size(544, 22);
+            this.statusStrip1.TabIndex = 2;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // toolStripStatusLabel1
+            // 
+            this.toolStripStatusLabel1.BackColor = System.Drawing.SystemColors.Control;
+            this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+            this.toolStripStatusLabel1.Size = new System.Drawing.Size(60, 17);
+            this.toolStripStatusLabel1.Text = "Готово.    ";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(544, 280);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem addListToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem addElementToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem buttonEnumToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem objectEnumToolStripMenuItem;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
+    }
+}
+
---
+++ Lab3/Form1.cs
@@ -0,0 +1,238 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Media;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml.Linq;
+
+namespace Lab3
+{
+
+    public partial class Form1 : Form
+    {
+        MiniList<CheckBox> miniList;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void toolStripStatusLabel1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+
+        private void statusStrip1_ItemClicked(object sender, ToolStripItemClickedEventArgs e)
+        {
+
+        }
+
+        private bool _isBusy = false;
+        private string _lastStatusString = "";
+
+        private bool CheckNotBusyAndSet(string statusString)
+        {
+            bool flag = CheckNotBusy();
+            if (flag)
+            {
+                _lastStatusString = statusString;
+                toolStripStatusLabel1.Text = statusString;
+                Application.DoEvents();
+                _isBusy = true;
+            }
+            return flag;
+        }
+
+        private void addListToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (!CheckNotBusyAndSet("Создание списка")) return;
+            miniList = new MiniList<CheckBox>();
+            panel1.Controls.Clear();
+            toolStripStatusLabel1.Text = "Список создан.";
+            _isBusy = false;
+        }
+
+        private bool CheckNotBusy()
+        {
+            if (_isBusy)
+            {
+                toolStripStatusLabel1.Text = $"Дождитесь завершения комманды: {_lastStatusString}";
+                toolStripStatusLabel1.ForeColor = Color.Red;
+                Application.DoEvents();
+                SystemSounds.Exclamation.Play();
+                System.Threading.Thread.Sleep(100);
+                toolStripStatusLabel1.ForeColor = SystemColors.ControlText;
+                Application.DoEvents();
+                return false;
+            }
+            return true;
+        }
+
+        private void addElementToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (!CheckNotBusyAndSet("Добавление кнопки")) return;
+
+            if (miniList != null)
+            {
+                CheckBox checkBox = new CheckBox();
+                checkBox.Appearance = Appearance.Button;
+                checkBox.FlatStyle = FlatStyle.Standard;
+                checkBox.Top = 50;
+                checkBox.Width = 50;
+                checkBox.Left = 20 + checkBox.Width * miniList.Append(checkBox);
+                checkBox.Text = Convert.ToString(checkBox.Left);
+                this.panel1.Controls.Add(checkBox);
+                toolStripStatusLabel1.Text = "Добавлен элемент.";
+            }
+            else
+            {
+                toolStripStatusLabel1.Text = "Список не создан.";
+            }
+            _isBusy = false;
+             
+        }
+
+        private void buttonEnumToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+            if (!CheckNotBusyAndSet("Перебор кнопок")) return;
+
+            if (panel1.Controls.Count == 0)
+            {
+                toolStripStatusLabel1.Text = "На панели нет элементов.";
+                _isBusy = false;
+                return;
+            }
+
+            foreach (var cb in panel1.Controls.OfType<CheckBox>().OrderBy(c => c.Left))
+            {
+                var old = cb.BackColor;
+                cb.Checked = true;
+                //toolStripStatusLabel1.Text = $"Перебор кнопок: значение Left={cb.Left}";
+                Application.DoEvents();
+                System.Threading.Thread.Sleep(160);
+                cb.Checked = false;
+            }
+            toolStripStatusLabel1.Text = "Перебор кнопок завершён.";
+            _isBusy = false;
+        }
+
+        private void objectEnumToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (!CheckNotBusyAndSet("Перебор объектов")) return;
+
+            if (miniList == null || miniList.Top == null)
+            {
+                toolStripStatusLabel1.Text = "Список пуст.";
+                _isBusy = false;
+                return;
+            }
+
+            foreach (var cb in miniList)
+            {
+                var old = cb.BackColor;
+                cb.Checked = true;
+                //toolStripStatusLabel1.Text = $"Перебор объектов списка: значение Left={cb.Left}";
+                Application.DoEvents(); 
+                System.Threading.Thread.Sleep(160);
+                cb.Checked = false;
+            }
+
+            toolStripStatusLabel1.Text = $"Перебор объектов списка завершён.";
+            _isBusy = false;
+        }
+    }
+
+    class MiniList<T> : IEnumerable<T>
+    {
+        int num = 0;
+        public Node Top;
+        public class Node
+        {
+            public T Content;
+            public Node Next = null;
+        }
+        public int Append(T s)
+        {
+            Node p = new Node();
+            p.Content = s;
+            if (Top != null) p.Next = Top;
+            Top = p;
+            return num++;
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            return new MiniEnumerator<T>(this);
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+
+    class MiniEnumerator<T> : IEnumerator<T>
+    {
+        private readonly MiniList<T> _list;
+        private MiniList<T>.Node _beforeFirst;
+        private MiniList<T>.Node _current;
+        private bool _disposed;
+
+        public MiniEnumerator(MiniList<T> list)
+        {
+            _list = list ?? throw new ArgumentNullException(nameof(list));
+            _beforeFirst = new MiniList<T>.Node { Next = _list.Top };
+            _current = null;
+        }
+
+        public T Current
+        {
+            get
+            {
+                if (_current == null) throw new InvalidOperationException("Enumeration has not started.");
+                return _current.Content;
+            }
+        }
+
+        object IEnumerator.Current => Current;
+
+        public bool MoveNext()
+        {
+            if (_disposed) throw new ObjectDisposedException(nameof(MiniEnumerator<T>));
+
+            if (_current == null)
+            {
+                _current = _beforeFirst.Next;
+            }
+            else
+            {
+                _current = _current.Next;
+            }
+            return _current != null;
+        }
+
+        public void Reset()
+        {
+            if (_disposed) throw new ObjectDisposedException(nameof(MiniEnumerator<T>));
+            _current = null;
+        }
+
+        public void Dispose()
+        {
+            _disposed = true;
+        }
+    }
+}
